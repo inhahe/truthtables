4c7d90ff30333a326f85aec5229430e916d8ba44
inhahe <inhahe@gmail.com>
2016-02-18 16:05:53 -0500

    Initial directory upload

---
+++ truthtables.notes.py
@@ -0,0 +1,159 @@
+﻿
+# a implies b                 ->
+# a is implied by b        <-
+# a implies not-b         ->!
+# not-a is implied by b  !<-  (???)
+
+## a isn't implied by b          !<- (???)
+# a is implied by not-b         <-!  
+# not-a is implied by not-b  !<-!
+## a isn't implied by not-b          !<-!
+# not-a implies b                 !->
+## a doesn't imply b                !->  
+# not-a implies not-b          !->!
+## a doesn't imply not-b           !->! 
+
+# not-a doesn't imply b         !!->
+# not-a isn't implied by b      !!<-
+# not-a doesn't imply not-b   !!->!
+# not-a isn't implied by not-b   !!->!
+
+
+# or:
+## a isn't implied by b          /<-  or <!-   or </-
+
+
+# what is implied-by-not if False is default?
+
+
+ is-and-perhaps
+                  !<-
+                  could-be-but
+                  xor
+                  or 
+                  nor 
+                  xnor 
+                  could-be-and-not
+                  <-
+                  isn't-but-perhaps
+                  ->
+
+all names:
+
+false          or xand
+and 
+xleft   "exclusive left"
+left                                
+xright
+right                 
+xor                   
+or 
+nor 
+xnor 
+nright               
+xnleft
+nleft                 
+xnright
+nand 
+true             or xnand
+
+
+names = "false and  x◄  ◄ x►  ►  xor  or  nor  xnor n►  xn◄ n◄ xn► nand  true").split()
+
+
+or-not left 
+
+xnor is actually nxor!
+because xnor itself is the same as nor.  not_both(invert(or))).
+ alternatively it could be considered not_neither(invert(or)) which results in false.
+
+false is actually nxand, not xnand.
+
+all symbols: "^^ &  ->!   ◄  !<-    ►  ^      |  !|  !^  ►!  <-  !◄  ->  !&  !^^").split()
+
+all symbols: "^^ &  &!   ◄ 
+
+but then we change 'is implied by' to n!&  except that !& looks like nand 
+we change 'implies' to n&!         
+
+a==1 ◄ b==2                 a==1 !◄ b==2                 a==1 ►! b==2                 a==1 ►! b==2                 
+
+a==1 left b==3           a==1 !left b==3         a==1 right b==3           a==1 !right b==3         
+
+ x==1  <*   b==3             x==1  *>   b==3                 x==1  !<*   b==3             x==1  *>!   b==3
+
+a==1 <~ b==3               a==1 !<~ b==3           a==1 ~> b==3                 a==1 ~>! b==3
+
+x==1  p  b==3	     x==1  q  b==3                x==1  !p  b==3	     x==1  !q  b==3
+
+tables.update({"-/>":  3, "</-": 5, "</-!": 10, "!</-": 12})                             # e.g. 'not-a is true' as if that were 'not-a is not implied by b' as if that were '!</-'
+
+
+                                                                                                                    # or just use a/!a/b/!b, or !left, or !t[0], or !first, or "first-only" but that's just like could-be-and-not
+
+
+"!   &   !->    ◄  !<-  ►  ^  |  !|  !^    !►   <-    !◄  ->  !&  !!").split()
+
+names = "xand  and  n->  ◄ n<-  ►  xor  or  nor  xnor n►  <- n◄  ->  nand  xnand").split()
+
+
+
+
+# names = "false  and  n->  ◄  n<-  ►  xor  or  nor  xnor  n►  <-  n◄  ->  nand  true").split()
+# names = "n  &  n->  ◄  n<-  ►  ^  |  n|  n^  n►  <-  n◄  ->  n&  nn").split()
+# names = "n  &  x◄  ◄  x►  ►  ^  |  n|  n^  n►  xn►  n◄  xn◄  n&  nn").split()
+# names = "n  &  x◄  ◄  x►  ►  x|  |  n|  nx|  n►  xn►  n◄  xn◄  n&  nn").split()
+# names = "x&  &  x◄  ◄  x►  ►  x|  |  n|  nx|  n►  xn►  n◄  xn◄  n&  nx&").split()
+
+# names = "f  &  x◄  ◄  x►  ►  x|  |  n|  nx|  n►  xn►  n◄  xn◄  n&  t").split()
+# names = "n*  &  x◄  ◄  x►  ►  x|  |  n|  nx|  n►  xn►  n◄  xn◄  n&  *").split()
+# names = "0  &  x◄  ◄  x►  ►  x|  |  n|  nx|  n►  xn►  n◄  xn◄  n&  1").split()
+
+/[ny, y]    [n*, *]    /[nt, t]
+[n, y]      /[n, *]      [n, t]    /[n, 1]                n, * illogical
+[f, t]     /[0, 1]   
+[x, xn]  /[n, nn]
+[x&, nx&]  < logical
+
+[n*, *]
+[n, y]
+       [n, t]
+       [f, t]
+       [n, nn]
+[x, *]
+[x, y]
+       [x, t]
+       [x, nx]
+
+[n&, xn&]
+
+/[no, yes]     [false, true]   [xand, xnand]  /[never, always]   /[n, nn]   /[x, nx]   < latter two probably illogical
+
+
+ ^ or x|      # x| fewer symbols but ^ readily understood
+n-> or x◄    # x◄ fewer symbols, easier to understand, hard to type, -> is already standandized
+◄ or <   
+
+  # names = "nt  &  x◄  ◄  x►  ►  x|  |  n|  nx|  n►  xn►  n◄  xn◄  n&  t").split()-
+# names = "n  &  x◄  ◄  x►  ►  x|  |  n|  nx|  n►  xn►  n◄  xn◄  n&  y").split()
+# names = "n  &  x<  <  x>  >  x|  |  n|  nx|  n>  xn<  n<  xn<  n&  y").split()-
+
+# names = "n  &  x<  <  x>  >  ^  |  n|  x^  n>  nx<  n<  nx<  n&  y").split()-
+
+# names = "ny  &  x◄  ◄  x►  ►  x|  |  n|  nx|  n►  xn►  n◄  xn◄  n&  y").split()-
+
+
+# names = "false  and  n->  ◄  n<-  ►  xor  or  nor  xnor  n►  <-  n◄  ->  nand  true").split()
+# names = "x  &  n->  ◄  n<-  ►  ^  |  n|  n^  n►  <-  n◄  ->  n&  nx").split()
+# names = "x  &  x◄  ◄  x►  ►  ^  |  n|  n^  n►  xn►  n◄  xn◄  n&  nx").split()
+# names = "x  &  x◄  ◄  x►  ►  x|  |  n|  nx|  n►  xn►  n◄  xn◄  n&  nx").split()
+# names = "x  &  x◄  ◄  x►  ►  x|  |  n|  nx|  n►  xn►  n◄  xn◄  n&  nx").split()
+
+# names = "xand  and  xleft  left  xright  right  xor  or  nor  xnor  nright  xnleft  nleft  xnright  nand  xnand".split()
+# names = "no  and  xleft  left  xright  right  xor  or  nor  xnor  nright  xnleft  nleft  xnright  nand  yes".split()
+# names = "false  and  xleft  left  xright  right  xor  or  nor  xnor  nright  xnleft  nleft  xnright  nand  true".split()
+# names = "never  and  xleft  left  xright  right  xor  or  nor  xnor  nright  xnleft  nleft  xnright  nand  always".split()
+# names = "n  and  xleft  left  xright  right  xor  or  nor  xnor  nright  xnleft  nleft  xnright  nand  nn".split()
+# names = "x  and  xleft  left  xright  right  xor  or  nor  xnor  nright  xnleft  nleft  xnright  nand  nx".split()
+
+names = "false and  x◄  ◄ x►  ►  xor  or  nor  xnor n►  xn◄ n◄ xn► nand  true").split()
---
+++ truthtables.py
@@ -0,0 +1,95 @@
+﻿from getnames import *
+
+   #  you can pass any number of the following tokens to getnames() in any order. 
+   #
+   #          xand: use "xand" and "nxand" for the "false" and "true" gates, or "x&" and "nx&" if using symbols.
+   #      implies: use "implies" instead of "nxleft", "nimplied-by" instead of "xright", etc.
+   #          xleft: use "xleft" and "xright" instead of "nimplies" and "nimplied-by" (default unless implies is specified).
+   #    symbols: use symbolic names for logic gates, e.g. "&" instead of "and".
+   #    pointers: use "◄" and "►" instead of "<" and ">" when using symbols.  (symbols can still be typed in using < and >)
+   #     nocaret: don't use "^" for "xor" when using symbols.  use "x|" for "xor" and "nx|" for "xnor".
+   #             "!": use "!" instead of "n" in names when using symbols.
+   #    true and false are keyword-only parameters. they set the names for the "true" and "false" gates when using symbols.
+   #      possible false values:     f, n,         "n*", x,     "x&"
+   #      possible true values:      t, nn, y,    "*", nx,    "nx&", "xn&", "!x&", "x!&"
+   #      not every possible combination of "true" vs. "false" values can be used..
+   #
+   #  example: 
+   #    names = getnames(symbols, nocaret, pointers, "!", true="x&")
+
+#names, lookup = getnames(), getlookup()
+
+names = "xand nor xleft nleft xright nright xor nand and nxor right nxright left nxleft or nxand".split()
+lookup = dict(map(reversed, enumerate(names)))
+
+prompt = "<logic gate> <operator> <logic gate>: "
+
+print
+while 1:
+  try:
+    tables = table1, table2, table3 = raw_input(prompt).lower().split()
+    t1, t2, t3 = map(lookup.get, tables)
+  except:
+    raise SystemExit
+  
+  r = sum((
+     x * 
+       (
+         (
+         t2 >> 
+           (
+             ( (  (t3 & x) > 0) *2 )
+             | 
+             ((t1 & x) > 0)
+           )
+         ) 
+       > 0
+       ) 
+     for x in (1, 2, 4, 8)
+             
+))
+
+   # study order of ops to get rid of parenthesis
+  print names[r]
+
+
+#  print '%s %s %s == %s' % (names[rtable1
+
+
+# kup[table1].capitalize(), lookup[table2], lookup[table3].capitalize(), names[r].capitalize())
+
+# should we look up all the names instead of using the input?
+
+
+
+
+"""
+1 1 0 0  b
+1 0 1 0  a
+---------             
+0 0 0 0        xand
+0 0 0 1        nor
+0 0 1 0        xleft
+0 0 1 1        nleft
+0 1 0 0        xright
+0 1 0 1        nright
+0 1 1 0        xor
+0 1 1 1        nand
+1 0 0 0        and
+1 0 0 1        nxor
+1 0 1 0        right
+1 0 1 1        nxright
+1 1 0 0        left
+1 1 0 1        nxleft
+1 1 1 0        or
+1 1 1 1        nxand
+"""
+
+
+
+
+
+
+
+
+
